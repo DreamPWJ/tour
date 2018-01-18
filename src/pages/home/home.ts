61bc943d5f05b1d9280954f3d6128f54b6f647a0
406798106@qq.com <406798106@qq.com>
2018-01-18 08:40:23 +0800

    自驾游APP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import ECharts from 'echarts';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('chart') chart: ElementRef;
    backgroundImage = 'assets/imgs/main/home-bg.jpg';
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidEnter() {
+    this.initChart();
+  }
+
+  initChart() {
+    let element = this.chart.nativeElement;
+    element.style.width = (document.body.clientWidth - 16) + 'px';//设置容器宽度
+    let myChart = ECharts.init(element);
+    myChart.setOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+      },
+      legend: {
+        orient: 'vertical',
+        x: 'left',
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: ['50%', '70%'],
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
+              }
+            }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
+          },
+          data: [
+            {value: 335, name: '直接访问'},
+            {value: 310, name: '邮件营销'},
+            {value: 234, name: '联盟广告'},
+            {value: 135, name: '视频广告'},
+            {value: 1548, name: '搜索引擎'}
+          ]
+        }
+      ]
+    });
   }
 
 }
