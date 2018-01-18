bffdd554d280804ad8bb4e4fe05e8738c5a03316
406798106@qq.com <406798106@qq.com>
2018-01-18 10:28:53 +0800

    自驾游APP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
 import ECharts from 'echarts';
 
 @Component({
@@ -8,7 +8,8 @@ import ECharts from 'echarts';
 })
 export class HomePage {
   @ViewChild('chart') chart: ElementRef;
-   backgroundImage = 'assets/imgs/main/home-bg.jpg';
+  backgroundImage = 'assets/imgs/main/home-bg.jpg';
+
   constructor(public navCtrl: NavController) {
 
   }
@@ -19,49 +20,130 @@ export class HomePage {
 
   initChart() {
     let element = this.chart.nativeElement;
-    element.style.width = (document.body.clientWidth - 16) + 'px';//设置容器宽度
+    /*    element.style.width = (document.body.clientWidth - 16) + 'px';//设置容器宽度*/
+    var data = [
+      {name: '成都', value: 80},
+      {name: '武汉', value: 50},
+
+    ];
+    var geoCoordMap = {
+      '成都': [104.06, 30.67],
+      '武汉': [114.31, 30.52],
+
+    };
+
+    var convertData = function (data) {
+      var res = [];
+      for (var i = 0; i < data.length; i++) {
+        var geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+          res.push({
+            name: data[i].name,
+            value: geoCoord.concat(data[i].value)
+          });
+        }
+      }
+      return res;
+    };
     let myChart = ECharts.init(element);
     myChart.setOption({
+      backgroundColor: '',
+      title: {
+        text: '',
+        subtext: '',
+        sublink: '',
+        left: 'center',
+        textStyle: {
+          color: '#fff'
+        }
+      },
       tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b}: {c} ({d}%)"
+        trigger: 'item'
       },
       legend: {
         orient: 'vertical',
-        x: 'left',
-        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        y: 'bottom',
+        x: 'right',
+        data: [''],
+        textStyle: {
+          color: '#fff'
+        }
+      },
+      geo: {
+        map: 'china',
+        label: {
+          emphasis: {
+            show: false
+          }
+        },
+        center: [115.97, 29.71],
+        zoom:0.3,
+        roam: true,
+        itemStyle: {
+          normal: {
+            areaColor: '#323c48',
+            borderColor: '#111'
+          },
+          emphasis: {
+            areaColor: '#2a333d'
+          }
+        }
       },
       series: [
         {
-          name: '访问来源',
-          type: 'pie',
-          radius: ['50%', '70%'],
-          avoidLabelOverlap: false,
+          name: 'pm2.5',
+          type: 'scatter',
+          coordinateSystem: 'geo',
+          data: convertData(data),
+          symbolSize: function (val) {
+            return val[2] / 10;
+          },
           label: {
             normal: {
-              show: false,
-              position: 'center'
+              formatter: '{b}',
+              position: 'right',
+              show: false
             },
             emphasis: {
-              show: true,
-              textStyle: {
-                fontSize: '30',
-                fontWeight: 'bold'
-              }
+              show: true
             }
           },
-          labelLine: {
+          itemStyle: {
             normal: {
-              show: false
+              color: '#ddb926'
+            }
+          }
+        },
+        {
+          name: 'Top 5',
+          type: 'effectScatter',
+          coordinateSystem: 'geo',
+          data: convertData(data.sort(function (a, b) {
+            return b.value - a.value;
+          }).slice(0, 6)),
+          symbolSize: function (val) {
+            return val[2] / 10;
+          },
+          showEffectOn: 'render',
+          rippleEffect: {
+            brushType: 'stroke'
+          },
+          hoverAnimation: true,
+          label: {
+            normal: {
+              formatter: '{b}',
+              position: 'right',
+              show: true
             }
           },
-          data: [
-            {value: 335, name: '直接访问'},
-            {value: 310, name: '邮件营销'},
-            {value: 234, name: '联盟广告'},
-            {value: 135, name: '视频广告'},
-            {value: 1548, name: '搜索引擎'}
-          ]
+          itemStyle: {
+            normal: {
+              color: '#f4e925',
+              shadowBlur: 10,
+              shadowColor: '#333'
+            }
+          },
+          zlevel: 1
         }
       ]
     });
