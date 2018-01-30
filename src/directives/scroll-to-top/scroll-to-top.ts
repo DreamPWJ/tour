0a6646f524598e16c3bfe1b624f823e59ca7f879
406798106@qq.com <406798106@qq.com>
2018-01-30 10:17:01 +0800

    自驾游APP

--- src/directives/scroll-to-top/scroll-to-top.ts
+++ src/directives/scroll-to-top/scroll-to-top.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, ViewChild} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 import {Content} from "ionic-angular";
 
 /**
