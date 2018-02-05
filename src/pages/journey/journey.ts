046f331ce1c3b721a6299afd828fedd359a1b995
406798106@qq.com <406798106@qq.com>
2018-02-05 22:22:00 +0800

    自驾游APP

--- src/pages/journey/journey.ts
+++ src/pages/journey/journey.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams, Slides} from 'ionic-angular';
+import {AppService} from "../../providers/util/app.service";
 
 
 
@@ -9,12 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'journey.html',
 })
 export class JourneyPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild("slides") slides: Slides;
+/*  slidesLength: number = 0;
+  currentIndex: number = 1;*/
+  slidesData:any=[{
+    title:"自驾游",
+    img:'assets/imgs/journey/2.png',
+    price:16999
+    } ,
+    {
+      title:"租车",
+      img:'assets/imgs/journey/1.png',
+      price:280
+    }]
+  constructor(public navCtrl: NavController, public navParams: NavParams,public  appService:AppService) {
   }
 
   ionViewDidLoad() {
+    this.appService.loadingShow();
+  }
 
+  ionViewWillEnter(){
+    this.appService.loadingHide();
+
+  }
+
+  ionViewDidEnter(){
+/*    this.slidesLength = this.slides.length()*/
+  }
+
+  slideChanged() {
+/*    this.currentIndex = this.slides.getActiveIndex() + 1;*/
   }
 
 }
