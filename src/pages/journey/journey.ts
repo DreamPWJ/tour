59bf2208b5128af59205128696f6361e8e0d3902
406798106@qq.com <406798106@qq.com>
2018-02-06 10:32:40 +0800

    自驾游APP

--- src/pages/journey/journey.ts
+++ src/pages/journey/journey.ts
@@ -16,12 +16,19 @@ export class JourneyPage {
   slidesData:any=[{
     title:"自驾游",
     img:'assets/imgs/journey/2.png',
-    price:16999
-    } ,
+    price:16999,
+    time:"2018.03.15",
+    header:{title:"【最美芝林2日】",subTitle:"桃花季预售·雅鲁藏布大峡谷·嘎拉桃花村/拉如桃花村",isImg:false,imgUrl:""},
+    details:[{title:'时间',content:'2018.03.15  - 2018.03.17'},{title:'类型',content:'豪华游'},{title:'人数',content:'成人·2   拼车·2'},{title:'服务',content:'司机·1  地接·1  车辆·1'}]
+
+  } ,
     {
       title:"租车",
       img:'assets/imgs/journey/1.png',
-      price:280
+      price:280,
+      time:"2018.05.15",
+      header:{title:"雪佛兰迈锐宝",subTitle:"三厢1.5T自动乘坐5人",isImg:true,imgUrl:"assets/imgs/journey/car.png"},
+      details:[{title:'时间',content:'03.15  10:00  -  03.17  10:00'},{title:'数量',content:'1辆'},{title:'取车',content:'成都·电子科大维修中心'},{title:'还车',content:'成都·电子科大维修中心'},{title:'服务',content:'上门取送'}]
     }]
   constructor(public navCtrl: NavController, public navParams: NavParams,public  appService:AppService) {
   }
