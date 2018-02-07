a12dbd6cbdec8631bd3f61f8b0cf7f9dd0725c4c
dreamtu <1060138309@qq.com>
2018-02-07 17:26:47 +0800

    自驾游行程

--- src/pages/journey/self-tour-journey/self-tour-journey.ts
+++ src/pages/journey/self-tour-journey/self-tour-journey.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AppService} from "../../../providers/util/app.service";
+import "../../../assets/js/map.js";
 
 /**
  * Generated class for the SelfTourJourneyPage page.
@@ -7,6 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+declare let AMap;
 
 @IonicPage()
 @Component({
@@ -14,12 +17,75 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'self-tour-journey.html',
 })
 export class SelfTourJourneyPage {
+  @ViewChild('maps') maps: ElementRef;
+  map: any;//地图对象
+  dateFlg:number = 1;//日期选择flg
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  selfJourList = [
+    {gongli:'出发',index:1,date:'全部',name:'布达拉宫白塔',position:'93.600738,30.054808',icon:'assets/imgs/journey/marker1.png',img1:'assets/imgs/self-tour/china1.png',content:'出发'},
+    {gongli:'-93公里-',index:2,date:'2018.03.15',name:'米拉山口',position:'93.021923,30.001119',icon:'assets/imgs/journey/marker2.png',img1:'assets/imgs/self-tour/china2.png',content:'景点 建议停留:30分钟'},
+    {gongli:'-93公里-',index:3,date:'2018.03.16',name:'雅鲁藏布大峡谷',position:'94.352642,29.641304',icon:'assets/imgs/journey/marker3.png',img1:'assets/imgs/self-tour/china3.png',content:'景点'},
+    {gongli:'-93公里-',index:4,date:'2018.03.17',name:'珠穆朗玛峰',position:'94.995342,29.788007',icon:'assets/imgs/journey/marker4.png',img1:'assets/imgs/self-tour/china4.png',content:'景点'},
+  ];
+  constructor(public navCtrl: NavController, public navParams: NavParams,public appService: AppService) {
   }
 
   ionViewDidLoad() {
+    this.appService.loadingShow();
+  }
+
+  ionViewDidEnter() {
+    this.appService.loadingHide();
+    this.map = new AMap.Map(this.maps.nativeElement, {
+      view: new AMap.View2D({//创建地图二维视口
+        zoom: 8, //设置地图缩放级别
+        center:[93.600738,30.054808],
+        rotateEnable: true,
+        showBuildingBlock: true
+      })
+    });
+    let markers = []; //标记
+    for (let i = 0; i < this.selfJourList.length; i += 1) {
+      let marker;
+      marker = new AMap.Marker({
+        offset: new AMap.Pixel(-12,-12),
+        zIndex: 101,
+        title: this.selfJourList[i].name,
+        extData:this.selfJourList[i].index,
+        position: this.selfJourList[i].position.split(','),
+        icon: this.selfJourList[i].icon,
+        // content: '<div class="">1</div>' , //自定义点标记覆盖物内容
+        map: this.map
+      });
+      markers.push(marker);
+      marker.on('click',function(e){
+        // marker.setContent(marker.getTitle());
+        // 设置label标签
+        marker.setLabel({//label默认蓝框白底左上角显示，样式className为：amap-marker-label
+          offset: new AMap.Pixel(15, 2),//修改label相对于maker的位置
+          content: marker.getTitle()
+        });
+      });
+    }
+
+    //连线 绘制轨迹
+    let lineArr = [];
+    for (let i = 0; i < this.selfJourList.length; i++) {
+      lineArr.push(this.selfJourList[i].position.split(','));
+    }
+
+    let polyline = new AMap.Polyline({
+      map: this.map,
+      path: lineArr,
+      strokeColor: "#6aeff3",  //线颜色
+      strokeOpacity: 0.8,     //线透明度
+      strokeWeight: 3,      //线宽
+      strokeStyle: "solid"  //线样式
+    });
 
   }
 
+  dateSelect(index:number){
+    this.dateFlg = index;
+  }
 }
