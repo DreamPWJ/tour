ddd8076c8d2d1271f17a18238e8fb7d48de0f0db
dreamtu <1060138309@qq.com>
2018-02-28 22:06:07 +0800

    跨城特惠

--- src/pages/rent-car/cross-city/cross-city.ts
+++ src/pages/rent-car/cross-city/cross-city.ts
@@ -14,12 +14,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cross-city.html',
 })
 export class CrossCityPage {
+  wrapFlg:boolean = true;
+  crossList = [
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'},
+    {index:1,gocity:'成都',endcity:'三亚',goday:'02.23 周五',endday:'02.26 周一',price:'99',time:"4",cartype:'大众朗逸',img:'assets/imgs/rent-car/car.png'}
+  ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CrossCityPage');
+  wrapStyle(){
+    if(this.wrapFlg){
+      this.wrapFlg = false;
+    }else{
+      this.wrapFlg = true;
+    }
   }
 
 }
