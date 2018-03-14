8c7933b25e9988beeef3a08246aaf0c555422118
dreamtu <1060138309@qq.com>
2018-03-14 10:31:59 +0800

    车型选择日历

--- src/pages/rent-car/car-detail/car-detail.ts
+++ src/pages/rent-car/car-detail/car-detail.ts
@@ -17,9 +17,49 @@ export class CarDetailPage {
   cardetailArr:string[]=[ '订车流程','必备材料' ,'租车须知','注意事项'];
   cardetailSeg:string=this.cardetailArr[1];
 
+  calendarList = [
+    {date:'',price:'',selectFlg:null},
+    {date:'',price:'',selectFlg:null},
+    {date:'',price:'',selectFlg:null},
+    {date:'',price:'',selectFlg:null},
+    {date:'',price:'',selectFlg:null},
+    {date:'1',price:'109',selectFlg:true},
+    {date:'2',price:'99',selectFlg:false},
+    {date:'3',price:'99',selectFlg:false},
+    {date:'4',price:'79',selectFlg:false},
+    {date:'5',price:'79',selectFlg:false},
+    {date:'6',price:'79',selectFlg:false},
+    {date:'7',price:'99',selectFlg:false},
+    {date:'8',price:'99',selectFlg:false},
+    {date:'9',price:'109',selectFlg:false},
+    {date:'10',price:'79',selectFlg:false},
+    {date:'11',price:'89',selectFlg:false},
+    {date:'12',price:'79',selectFlg:false},
+    {date:'13',price:'79',selectFlg:false},
+    {date:'14',price:'79',selectFlg:false},
+    {date:'15',price:'79',selectFlg:false},
+    {date:'16',price:'99',selectFlg:false},
+    {date:'17',price:'99',selectFlg:false},
+    {date:'18',price:'99',selectFlg:false}
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   segmentChanged(event){}
 
+  //日历选择
+  calendarCli(date){
+    if(date!=''){
+      for(let i =0;i<this.calendarList.length;i++){
+        if(this.calendarList[i].date == date){
+          if(this.calendarList[i].selectFlg){
+            this.calendarList[i].selectFlg = false;
+          }else {
+            this.calendarList[i].selectFlg = true;
+          }
+        }
+      }
+    }
+  }
 }
