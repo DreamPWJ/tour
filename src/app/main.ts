12b42681efeb70110fc5ebb7793daf1d967b7fa0
406798106@qq.com <406798106@qq.com>
2018-05-30 13:35:04 +0800

    增加web端启动动画

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
+/*main.ts 入口文件 只调用一个入口文件，让入口文件帮我们去引入其它依赖的文件*/
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(()=>{
+  //启动完成  web端启动页面隐藏
+  document.querySelector('.preloader').remove();
+});
